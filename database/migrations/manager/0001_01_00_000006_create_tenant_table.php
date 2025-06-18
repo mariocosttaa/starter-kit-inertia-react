b61b40f98771a77cbc316a40169bfa213cc42582
Mário Costa <mariocostaa6@gmail.com>
2025-06-18 02:22:01 +0100

    Fix - Rename tenant table to tenants in migration

--- database/migrations/manager/0001_01_00_000006_create_tenant_table.php
+++ database/migrations/manager/0001_01_00_000006_create_tenant_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('tenant', function (Blueprint $table) {
+        Schema::create('tenants', function (Blueprint $table) {
             $table->id()->startingValue(10000);
             $table->string('name');
             $table->string('slug')->unique()->index();
